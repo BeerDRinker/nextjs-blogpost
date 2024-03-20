e9cff37b876eac3baaa90786ee6e1647872ce99b
Rostyslav Palii <rostyslav.palii@l7informatics.com>
2024-03-20 16:41:47 +0200

    add add new post validation mesages to UI

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -10,15 +10,24 @@ export function PostForm({
   userSelectOptions: ReactNode;
 }) {
   const [error, formAction] = useFormState(createPostAction, {});
+
+  console.log("error: ", error);
+
   return (
     <div className="text-lg">
       <form action={formAction} className="mx-auto max-w-lg">
         <div className="mb-5">
           <div className="flex flex-col justify-between gap-4 sm:flex-row">
-            <div className="w-full sm:w-1/2">
+            <div className="relative mb-2 w-full sm:w-1/2">
               <label
                 htmlFor="title"
-                className="mb-2 block font-medium text-gray-900 dark:text-white"
+                className={`
+                  ${
+                    error.title
+                      ? "text-red-700 dark:text-red-500"
+                      : "text-gray-900 dark:text-white"
+                  } mb-2 block font-medium
+                `}
               >
                 Title
               </label>
@@ -26,10 +35,13 @@ export function PostForm({
                 type="text"
                 id="title"
                 name="title"
-                className="dark:shadow-sm-light block w-full rounded-lg border border-red-300 bg-gray-50 p-2.5 text-gray-900 shadow-sm focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                className={`dark:shadow-sm-light block w-full rounded-lg border p-2.5 shadow-sm dark:bg-gray-700 ${error.title ? " border-red-500 text-red-900 placeholder-red-700 focus:border-red-500 focus:ring-red-500 dark:border-red-500 dark:text-red-500 dark:placeholder-red-500 " : " text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600  dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 "}`}
                 placeholder="Post title..."
                 required
               />
+              <p className="absolute mt-1 text-sm text-red-600 dark:text-red-500">
+                {error.title}
+              </p>
             </div>
             <div className="w-full sm:w-1/2">
               <label
@@ -48,20 +60,31 @@ export function PostForm({
             </div>
           </div>
         </div>
-        <label
-          htmlFor="body"
-          className="mb-2 block font-medium text-gray-900 dark:text-white"
-        >
-          Body
-        </label>
-        <textarea
-          required
-          name="body"
-          id="body"
-          rows={8}
-          className="block w-full resize-none rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-          placeholder="Post body..."
-        ></textarea>
+        <div className="relative mb-2">
+          <label
+            htmlFor="body"
+            className={`
+                    ${
+                      error.body
+                        ? "text-red-700 dark:text-red-500"
+                        : "text-gray-900 dark:text-white"
+                    } mb-2 block font-medium
+                  `}
+          >
+            Body
+          </label>
+          <textarea
+            required
+            name="body"
+            id="body"
+            rows={8}
+            className={`dark:shadow-sm-light block w-full resize-none rounded-lg border p-2.5 shadow-sm dark:bg-gray-700 ${error.body ? " border-red-500 text-red-900 placeholder-red-700 focus:border-red-500 focus:ring-red-500 dark:border-red-500 dark:text-red-500 dark:placeholder-red-500 " : " text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600  dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 "}`}
+            placeholder="Post body..."
+          ></textarea>
+          <p className="absolute mt-1 text-sm text-red-600 dark:text-red-500">
+            {error.body}
+          </p>
+        </div>
         <div className="mt-4 flex justify-end gap-4">
           <Link className="rounded-lg bg-gray-500 px-4 py-2" href="/posts">
             Cancel
